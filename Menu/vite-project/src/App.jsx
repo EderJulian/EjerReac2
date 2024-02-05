a9be2a334d576d23cd584e270803f9e69aabd31c
Eder <julianeder97@gmail.com>
2024-02-05 14:29:24 +0100

    OlvideSubirlo

--- Menu/vite-project/src/App.jsx
+++ Menu/vite-project/src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
+import './App.css'
+import Header from './components/Header';
+import Home from './components/Home';
 
-const Counter = (props) => {
+function App() {
+  const MenuDelDia = [
+    {
+      id: 1,
+      name: 'plato 1',
+      description: 'Este es el plato 1',
+      price: 10,
+    },
+    {
+      id: 2,
+      name: 'plato 2',
+      description: 'Este es el plato 2',
+      price: 20,
+    },
+    {
+      id: 3,
+      name: 'plato 3',
+      description: 'Este es el plato 3',
+      price: 15,
+    },
+  ];
 
-const [counter, setCounter] = useState(props.initialValue);
+  return (
 
-const increment = () => {
+    <div>
 
-setCounter(counter + 1);
+      <Header />
+      <Home MenuDelDia={MenuDelDia} />
 
-};
+    </div>
+  )
+}
 
-return <button onClick={increment}>{counter}</button>;
-
-};
-
-export default Counter;
-
+export default App
